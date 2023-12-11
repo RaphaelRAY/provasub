edf020d6f3af941c859c36d9a4c5ca8511905fb1
Raphael Fernandes Raymundo <100390575+RaphaelRAY@users.noreply.github.com>
2023-12-11 09:56:57 -0300

    first commit

---
+++ src/main/java/main/main.java
@@ -0,0 +1,33 @@
+package main;
+
+import java.awt.HeadlessException;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        mainhash hash= new mainhash();
+        int escolha = 0;
+        try (Scanner Scanner = new Scanner(System.in)) {
+            while(escolha != 3){
+                System.out.println("1-Inserir\n2-Procurar\n3-sair\n");
+                escolha = Integer.valueOf(Scanner.nextLine());
+
+                switch (escolha) {
+                    case 1:
+                        System.out.print("Nome: ");
+                        hash.inseri_nome(Scanner.nextLine());
+
+                        break;
+                    case 2:
+                        System.out.print("Nome: ");
+                        String nome = Scanner.nextLine();
+                        hash.procurar_nome(nome);
+                        break;
+                }
+            }
+        } catch (NumberFormatException | HeadlessException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/main/mainhash.java
@@ -0,0 +1,77 @@
+package main;
+
+public class mainhash {
+    
+    Integer hashCode = null, chave;
+    String[] tabHash = new String[26];
+    
+    mainhash(){
+        /*for (int i = 0; i < tabHash.length; i++) {
+            tabHash[i] = new String(); 
+        }*/
+    }
+    void inseri_nome(String nome){
+        System.out.println();
+        // colocar o nome na tabela hash com rehashing
+
+        hashCode = hash(nome);
+        String chave = nome;
+
+        //System.out.println("Chave: " + chave + " HashCode: " + hashCode);
+
+        if (tabHash[hashCode] == null) {
+            tabHash[hashCode] = chave;
+        } else {
+            Integer posicao = rehashing(tabHash, chave);
+            if (posicao != null) {
+                tabHash[posicao] = chave;
+            } else {
+                System.out.println("Tabela Hash cheia!");
+            }
+        }
+            
+            
+            
+            
+        System.out.println("Indice\tNome");
+        for (int j = 0; j < tabHash.length; j++) {
+            if (tabHash[j] != null) {
+            System.out.print( j + " \t");
+            System.out.print(tabHash[j]);
+            System.out.println();
+            }
+        
+        }
+        System.out.println();
+    } 
+
+    void procurar_nome(String nome){
+
+
+    }
+
+
+
+    
+    public static Integer hash(String key) {
+        int numeroOrdemAlfabetica = key.length() ;
+        return numeroOrdemAlfabetica;
+    }
+    
+    
+    public static Integer rehashing(String[] tabHash2, String key) {
+        Integer hashCode = hash(key);
+        if (tabHash2[hashCode] == null) {
+            return hashCode;
+        } else {
+            for (int i = hashCode; i < tabHash2.length; i++) {
+                if (tabHash2[i] == null) {
+                    return i;
+                }
+            }
+        }
+        return null;
+    }
+
+    
+}
